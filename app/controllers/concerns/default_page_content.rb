4f192dd1462000ee1ab2ff94a4f25b9a9c7bcb9c
Jack <jdcorley@gmail.com>
2019-01-27 19:57:39 -0500

    updating portfolio nav about, and page title

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
   end
 
   def set_page_defaults
-    @page_title = "DevcampPortfolio | Jack's Portfolio Website"
+    @page_title = "Welcome | Jack's Portfolio Website"
     @seo_keywords = "Jack Corley's Portfolio"
   end
 end
